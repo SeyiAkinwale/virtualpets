92cf6df939e5345cf61bcdd830bd19cc40829666
SeyiAkinwale <72584469+SeyiAkinwale@users.noreply.github.com>
2023-04-24 19:25:04 +0000

    commenting all main

--- main.cpp
+++ main.cpp
@@ -91,21 +91,21 @@ void careFor(Pet* p, int d)
 
 int main()
 {
-    Pet* myPets[2];
-    myPets[0] = new Pet("Fluffy", 2);
-    myPets[1] = new Pet("Frisky", 4);
-    for (int day = 1; day <= 9; day++)
-    {
-        cout << "======= Day " << day << endl;
-        for (int k = 0; k < 2; k++)
-            careFor(myPets[k], day);
-    }
-    cout << "=======" << endl;
-    for (int k = 0; k < 2; k++)
-    {
-        if (myPets[k]->alive())
-            cout << "Animal Control has come to rescue "
-                 << myPets[k]->name() << endl;
-        delete myPets[k];
-    }
+//     Pet* myPets[2];
+//     myPets[0] = new Pet("Fluffy", 2);
+//     myPets[1] = new Pet("Frisky", 4);
+//     for (int day = 1; day <= 9; day++)
+//     {
+//         cout << "======= Day " << day << endl;
+//         for (int k = 0; k < 2; k++)
+//             careFor(myPets[k], day);
+//     }
+//     cout << "=======" << endl;
+//     for (int k = 0; k < 2; k++)
+//     {
+//         if (myPets[k]->alive())
+//             cout << "Animal Control has come to rescue "
+//                  << myPets[k]->name() << endl;
+//         delete myPets[k];
+//     }
 }
